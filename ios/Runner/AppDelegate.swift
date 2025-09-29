6606a2d939526cad886842e48cbb47a1f8ebca84
romjanali01673 <romjanali01673@gmail.com>
2025-09-29 23:10:40 +0600

    first commit

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,17 @@
+import Flutter
+import UIKit
+import flutter_background_service_ios // added this line reference of flutter background services
+
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // added this line reference of flutter background services
+    SwiftFlutterBackgroundServicePlugin.taskIdentifier = "your.custom.task.identifier"
+
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
---
+++ lib/presentation/p_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PHome extends StatefulWidget {
+  const PHome({super.key});
+
+  @override
+  State<PHome> createState() => _PHomeState();
+}
+
+class _PHomeState extends State<PHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
